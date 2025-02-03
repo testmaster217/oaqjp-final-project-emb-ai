797bf02c8db88f888d3f883f1015590f889553fd
Collin Vesel <cvesel217@gmail.com>
2025-02-02 21:34:55 -0500

    Modified server.py to pass PyLint's checks. Mostly I just added docstrings.

--- server.py
+++ server.py
@@ -1,3 +1,7 @@
+'''
+This is the code to run a Flask server for the emotion detector service.
+'''
+
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,6 +9,10 @@ app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def em_detector():
+    '''
+    This function applies the emotion detector to some text that is passed
+    in as a query parameter in an HTTP GET request.
+    '''
     text_to_analyze = request.args.get("textToAnalyze")
     result = emotion_detector(text_to_analyze)
 
@@ -15,11 +23,14 @@ def em_detector():
         'anger': {result['anger']}, 'disgust': {result['disgust']}, \
         'fear': {result['fear']}, 'joy': {result['joy']}, and 'sadness': \
         {result['sadness']}. The dominant emotion is {result['dominant_emotion']}."
-    
+
     return output
 
 @app.route("/")
 def render_index_page():
+    '''
+    This function renders the Djinja template for the emotion detector's webpage.
+    '''
     return render_template("index.html")
 
 if __name__ == "__main__":
