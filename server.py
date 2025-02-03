8a9172ee15bbd7706f93685db49660d05d7578dc
Collin Vesel <cvesel217@gmail.com>
2025-02-02 21:25:30 -0500

    Added the rest of the error-handling code to server.py.

--- server.py
+++ server.py
@@ -8,6 +8,9 @@ def em_detector():
     text_to_analyze = request.args.get("textToAnalyze")
     result = emotion_detector(text_to_analyze)
 
+    if result['dominant_emotion'] is None:
+        return "Invalid text! Please try again!"
+
     output = f"For the given statement, the system response is: \
         'anger': {result['anger']}, 'disgust': {result['disgust']}, \
         'fear': {result['fear']}, 'joy': {result['joy']}, and 'sadness': \
